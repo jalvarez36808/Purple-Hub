8b9f4991acf9b9a331daf27904c260f691fe0c1b
jalvarez36808 <150582723+jalvarez36808@users.noreply.github.com>
2025-01-23 00:40:22 -0600

    Test Commit

--- assets/js/loadHeader.js
+++ assets/js/loadHeader.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log('DOM fully loaded and parsed');
     fetch('../Components/header.html')
-        .then(response => response.text())
+        .then(response => {
+            console.log('Fetch response:', response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(data => {
+            console.log('Fetched data:', data);
             document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
         });
 });
