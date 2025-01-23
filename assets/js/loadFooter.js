ea878b486979dc3c3a198376f1a97711cf37325d
jalvarez36808 <150582723+jalvarez36808@users.noreply.github.com>
2025-01-23 02:07:46 -0600

    committing

--- assets/js/loadFooter.js
+++ assets/js/loadFooter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('../Components/footer.html')
+    fetch('footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
